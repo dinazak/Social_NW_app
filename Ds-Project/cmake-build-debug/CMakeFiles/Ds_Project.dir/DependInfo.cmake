d22f1471a536778d1515db6e9f9e62c445400d2d
dinazak <85263369+dinazak@users.noreply.github.com>
2022-06-28 16:48:21 +0200

    first

---
+++ Ds-Project/cmake-build-debug/CMakeFiles/Ds_Project.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/C++ Projects/Ds-Project/TreapNode.cpp" "D:/C++ Projects/Ds-Project/cmake-build-debug/CMakeFiles/Ds_Project.dir/TreapNode.cpp.obj"
+  "D:/C++ Projects/Ds-Project/User.cpp" "D:/C++ Projects/Ds-Project/cmake-build-debug/CMakeFiles/Ds_Project.dir/User.cpp.obj"
+  "D:/C++ Projects/Ds-Project/main.cpp" "D:/C++ Projects/Ds-Project/cmake-build-debug/CMakeFiles/Ds_Project.dir/main.cpp.obj"
+  "D:/C++ Projects/Ds-Project/myList.cpp" "D:/C++ Projects/Ds-Project/cmake-build-debug/CMakeFiles/Ds_Project.dir/myList.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Ds-Project/cmake-build-debug/CMakeFiles/Ds_Project.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ds_Project.dir/TreapNode.cpp.obj"
+  "CMakeFiles/Ds_Project.dir/User.cpp.obj"
+  "CMakeFiles/Ds_Project.dir/main.cpp.obj"
+  "CMakeFiles/Ds_Project.dir/myList.cpp.obj"
+  "Ds_Project.exe"
+  "Ds_Project.exe.manifest"
+  "Ds_Project.pdb"
+  "libDs_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ds_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
